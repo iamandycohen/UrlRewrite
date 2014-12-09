76ab20458866cada96fedd536029ce9701c2683a
Andy Cohen <iamandycohen@gmail.com>
2014-12-09 15:51:58 -0600

    made a few tests more specific and added a data class for initialization

---
+++ Hi.UrlRewrite.Tests/InboundRulesData.cs
@@ -0,0 +1,77 @@
+﻿using System.Web;
+using Hi.UrlRewrite.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hi.UrlRewrite.Tests
+{
+    public class InboundRulesData
+    {
+        public static List<InboundRule> GetInboundRulesData()
+        {
+            return new List<InboundRule>()
+            {
+                new InboundRule()
+                {
+                    Action = new RedirectAction()
+                    {
+                        AppendQueryString = true,
+                        HttpCacheability = HttpCacheability.NoCache,
+                        Name = "RedirectAction 1",
+                        RedirectType = RedirectType.Permanent,
+                        RewriteUrl = "http://www.google.com",
+                        StopProcessingOfSubsequentRules = false
+                    },
+                    Enabled = true,
+                    IgnoreCase = true,
+                    Name = "Inbound Rule 1",
+                    Pattern = "john",
+                    Using = Using.ExactMatch,
+                    RequestedUrl = RequestedUrl.MatchesThePattern,
+                    ConditionLogicalGrouping = LogicalGrouping.MatchAll
+                },
+                new InboundRule()
+                {
+                    Action = new RedirectAction()
+                    {
+                        AppendQueryString = true,
+                        HttpCacheability = HttpCacheability.NoCache,
+                        Name = "RedirectAction 1",
+                        RedirectType = RedirectType.Permanent,
+                        RewriteUrl = "http://{HTTP_HOST}/article.aspx?id={R:1}&amp;title={R:2}",
+                        StopProcessingOfSubsequentRules = false
+                    },
+                    Enabled = true,
+                    IgnoreCase = true,
+                    Name = "Inbound Rule 2",
+                    Pattern = "^article/([0-9]+)/([_0-9a-z-]+)",
+                    Using = Using.RegularExpressions,
+                    RequestedUrl = RequestedUrl.MatchesThePattern,
+                    ConditionLogicalGrouping = LogicalGrouping.MatchAll
+                },
+                new InboundRule()
+                {
+                    Action = new RedirectAction()
+                    {
+                        AppendQueryString = true,
+                        HttpCacheability = HttpCacheability.NoCache,
+                        Name = "RedirectAction 1",
+                        RedirectType = RedirectType.Permanent,
+                        RewriteUrl = "http://{HTTP_HOST}/hostreplaced",
+                        StopProcessingOfSubsequentRules = false
+                    },
+                    Enabled = true,
+                    IgnoreCase = true,
+                    Name = "Inbound Rule 3",
+                    Pattern = "^hostreplacement$",
+                    Using = Using.RegularExpressions,
+                    RequestedUrl = RequestedUrl.MatchesThePattern,
+                    ConditionLogicalGrouping = LogicalGrouping.MatchAll
+                }
+            };
+        }
+    }
+}
--- Hi.UrlRewrite.Tests/UrlRewriterTests.cs
+++ Hi.UrlRewrite.Tests/UrlRewriterTests.cs
@@ -17,66 +17,7 @@ namespace Hi.UrlRewrite.Tests
         [TestInitialize]
         public void Initialize()
         {
-            InboundRules = new List<InboundRule>()
-            {
-                new InboundRule()
-                {
-                    Action = new RedirectAction()
-                    {
-                        AppendQueryString = true,
-                        HttpCacheability = HttpCacheability.NoCache,
-                        Name = "RedirectAction 1",
-                        RedirectType = RedirectType.Permanent,
-                        RewriteUrl = "http://www.google.com",
-                        StopProcessingOfSubsequentRules = false
-                    },
-                    Enabled = true,
-                    IgnoreCase = true,
-                    Name = "Inbound Rule 1",
-                    Pattern = "john",
-                    Using = Using.ExactMatch,
-                    RequestedUrl = RequestedUrl.MatchesThePattern,
-                    ConditionLogicalGrouping = LogicalGrouping.MatchAll
-                },
-                new InboundRule()
-                {
-                    Action = new RedirectAction()
-                    {
-                        AppendQueryString = true,
-                        HttpCacheability = HttpCacheability.NoCache,
-                        Name = "RedirectAction 1",
-                        RedirectType = RedirectType.Permanent,
-                        RewriteUrl = "http://{HTTP_HOST}/article.aspx?id={R:1}&amp;title={R:2}",
-                        StopProcessingOfSubsequentRules = false
-                    },
-                    Enabled = true,
-                    IgnoreCase = true,
-                    Name = "Inbound Rule 2",
-                    Pattern = "^article/([0-9]+)/([_0-9a-z-]+)",
-                    Using = Using.RegularExpressions,
-                    RequestedUrl = RequestedUrl.MatchesThePattern,
-                    ConditionLogicalGrouping = LogicalGrouping.MatchAll
-                },
-                new InboundRule()
-                {
-                    Action = new RedirectAction()
-                    {
-                        AppendQueryString = true,
-                        HttpCacheability = HttpCacheability.NoCache,
-                        Name = "RedirectAction 1",
-                        RedirectType = RedirectType.Permanent,
-                        RewriteUrl = "http://{HTTP_HOST}/tested",
-                        StopProcessingOfSubsequentRules = false
-                    },
-                    Enabled = true,
-                    IgnoreCase = true,
-                    Name = "Inbound Rule 3",
-                    Pattern = "^test$",
-                    Using = Using.RegularExpressions,
-                    RequestedUrl = RequestedUrl.MatchesThePattern,
-                    ConditionLogicalGrouping = LogicalGrouping.MatchAll
-                }
-            };
+            InboundRules = InboundRulesData.GetInboundRulesData();
         }
 
         [TestMethod]
@@ -95,7 +36,7 @@ namespace Hi.UrlRewrite.Tests
         }
 
         [TestMethod]
-        public void ProcessRequestUrlWithCaptureGroupsTest()
+        public void ProcessRequestUrlWithCaptureGroups()
         {
             var rewriter = new UrlRewriter();
             var rewriteResult = rewriter.ProcessRequestUrl(new Uri("http://fictitioussite.com/article/1/2"), InboundRules);
@@ -106,12 +47,12 @@ namespace Hi.UrlRewrite.Tests
         }
 
         [TestMethod]
-        public void ProcessRequestUrlWithHttpHostReplacementTest()
+        public void ProcessRequestUrlWithHttpHostReplacement()
         {
             var rewriter = new UrlRewriter();
-            var rewriteResult = rewriter.ProcessRequestUrl(new Uri("http://fictitioussite.com/test"), InboundRules);
+            var rewriteResult = rewriter.ProcessRequestUrl(new Uri("http://fictitioussite.com/hostreplacement"), InboundRules);
 
-            var expectedUri = new Uri("http://fictitioussite.com/tested");
+            var expectedUri = new Uri("http://fictitioussite.com/hostreplaced");
 
             Assert.AreEqual(expectedUri, rewriteResult.RewrittenUri);
         }
