7c0754cb95abd38e2e582b82a916c91c864f4903
Andy Cohen <iamandycohen@gmail.com>
2014-12-17 21:05:10 -0600

    start of rules cache

---
+++ Hi.UrlRewrite/Caching/RulesCache.cs
@@ -0,0 +1,28 @@
+﻿using Hi.UrlRewrite.Entities.Rules;
+using Sitecore;
+using Sitecore.Caching;
+using Sitecore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hi.UrlRewrite.Caching
+{
+    public class RulesCache : CustomCache
+    {
+        private Database _db;
+
+        public RulesCache(Database db) : 
+            base(string.Format("Hi.UrlRewrite[{0}]", db.Name), StringUtil.ParseSizeString("10MB"))
+        {
+            _db = db;
+        }
+
+        //public List<InboundRule> GetInboundRules()
+        //{
+            
+        //}
+
+    }
+}
