c6d0fd8f54f4c0ca438d374f606b978bbd3f69ab
Andy Cohen <iamandycohen@gmail.com>
2015-01-11 21:42:39 -0600

    start of reporting job

---
+++ Hi.UrlRewrite/Jobs/ReportingService.cs
@@ -0,0 +1,32 @@
+﻿using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hi.UrlRewrite.Jobs
+{
+    public class ReportingService
+    {
+
+        public Job StartJob()
+        {
+            var jobName = string.Format("UrlRewrite::Reporting - Saving rewrite info");
+
+            JobOptions options = new JobOptions(jobName,
+                                                "UrlRewrite",
+                                                Context.Site.Name,
+                                                this,
+                                                "SaveRewriteInfo",
+                                                new object[] {  });
+            return JobManager.Start(options);
+        }
+
+        public void SaveRewriteInfo(Item root)
+        {
+           
+        }
+    }
+}
