ef6cdfcefd62dc1ac7260f42c1446c8d57f27271
Andy Cohen <iamandycohen@gmail.com>
2015-02-13 19:54:53 -0600

    added exception handling around installation process

--- Hi.UrlRewrite/Processing/Setup.cs
+++ Hi.UrlRewrite/Processing/Setup.cs
@@ -1,4 +1,6 @@
-﻿using Hi.UrlRewrite.Templates.Settings;
+﻿using System;
+using Hi.UrlRewrite.Templates.Settings;
+using Sitecore;
 using Sitecore.Configuration;
 using Sitecore.Data;
 using Sitecore.Publishing;
@@ -11,25 +13,32 @@ namespace Hi.UrlRewrite.Processing
     {
         public void InstallItems()
         {
-            var settingsDb = Database.GetDatabase("master");
-            if (settingsDb == null) return;
-
-            using (new SecurityDisabler())
+            try
             {
-                var settingsItem = settingsDb.GetItem(new ID(Constants.UrlRewriteSettings_ItemId));
-                if (settingsItem == null) return;
+                var settingsDb = Factory.GetDatabase("master");
+                if (settingsDb == null) return;
 
-                var settings = new SettingsItem(settingsItem);
-                var publishingTargets = settings.InstallationPublishingTargets.GetItems();
-                var dbArray = publishingTargets.Select(x => Factory.GetDatabase(x.Fields["Target database"].Value)).ToArray();
-
-                // install templates
-                var urlRewriteTemplatesFolderItem = settingsDb.GetItem(new ID(Constants.UrlRewriteTemplatesFolder_ItemId));
-                if (urlRewriteTemplatesFolderItem != null)
+                using (new SecurityDisabler())
                 {
-                    PublishManager.PublishItem(urlRewriteTemplatesFolderItem, dbArray, urlRewriteTemplatesFolderItem.Languages, true,
-                        true);
+                    var settingsItem = settingsDb.GetItem(new ID(Constants.UrlRewriteSettings_ItemId));
+                    if (settingsItem == null) return;
+
+                    var settings = new SettingsItem(settingsItem);
+                    var publishingTargets = settings.InstallationPublishingTargets.GetItems();
+                    var dbArray = publishingTargets.Select(x => Factory.GetDatabase(x.Fields["Target database"].Value)).ToArray();
+
+                    // install templates
+                    var urlRewriteTemplatesFolderItem = settingsDb.GetItem(new ID(Constants.UrlRewriteTemplatesFolder_ItemId));
+                    if (urlRewriteTemplatesFolderItem != null)
+                    {
+                        PublishManager.PublishItem(urlRewriteTemplatesFolderItem, dbArray, urlRewriteTemplatesFolderItem.Languages, true,
+                            true);
+                    }
                 }
+            }
+            catch (Exception)
+            {
+                Log.Debug(this, "Unable to run installation.");
             }
         }
     }
