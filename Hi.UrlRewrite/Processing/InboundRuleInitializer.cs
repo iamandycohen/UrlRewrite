976a7d227a6c1156a64fd8a2df6bcd81e25b5f00
iamandycohen <iamandycohen@gmail.com>
2015-10-28 10:47:39 -0500

    added initialization of workflow deployment and rebuil tthe package

--- Hi.UrlRewrite/Processing/InboundRuleInitializer.cs
+++ Hi.UrlRewrite/Processing/InboundRuleInitializer.cs
@@ -19,11 +19,17 @@ namespace Hi.UrlRewrite.Processing
             {
                 using (new SecurityDisabler())
                 {
+
+                    // cache all of the rules
+
                     foreach (var db in Factory.GetDatabases().Where(e => e.HasContentItem))
                     {
                         var rulesEngine = new RulesEngine(db);
                         rulesEngine.GetCachedInboundRules();
                     }
+
+                    // make sure that the page event has been deployed
+                    DeployEventIfNecessary();
                 }
             }
             catch (Exception ex)
@@ -31,5 +37,49 @@ namespace Hi.UrlRewrite.Processing
                 Hi.UrlRewrite.Log.Error(this, ex, "Exception during initialization.");
             }
         }
+
+        private void DeployEventIfNecessary()
+        {
+            var database = Sitecore.Data.Database.GetDatabase("master");
+            if (database == null)
+            {
+                return;
+            }
+
+            var eventItem = database.GetItem(new ID(Constants.RedirectEventItemId));
+            if (eventItem == null)
+            {
+                return;
+            }
+
+            var workflow = database.WorkflowProvider.GetWorkflow(eventItem);
+            if (workflow == null)
+            {
+                return;
+            }
+
+            var workflowState = workflow.GetState(eventItem);
+            if (workflowState == null)
+            {
+                return;
+            }
+
+            const string analyticsDraftStateWorkflowId = "{39156DC0-21C6-4F64-B641-31E85C8F5DFE}";
+
+            if (!workflowState.StateID.Equals(analyticsDraftStateWorkflowId))
+            {
+                return;
+            }
+
+            const string analyticsDeployCommandId = "{4044A9C4-B583-4B57-B5FF-2791CB0351DF}";
+            var workflowResult = workflow.Execute(analyticsDeployCommandId, eventItem, "Deploying UrlRewrite Redirect event during initialization", false, new object[0]);
+
+        }
+
+
+
     }
+
+
+
 }
